46b4580b0113e650e656afd5cf21ef117bb783c7
Louis Narmour <louis.narmour@colostate.edu>
2020-12-02 22:11:40 +0000

    fix generate verify bug when using TiledCodeGenOptions

--- bundles/edu.csu.melange.alphaz.commands.scripts.mde/src-gen/edu/csu/melange/alphaz/commands/scripts/GenerateVerificationCode.java
+++ bundles/edu.csu.melange.alphaz.commands.scripts.mde/src-gen/edu/csu/melange/alphaz/commands/scripts/GenerateVerificationCode.java
@@ -1,10 +1,27 @@
 package edu.csu.melange.alphaz.commands.scripts;
 import fr.irisa.r2d2.gecos.framework.IModuleArgumentName;
 import org.polymodel.polyhedralIR.Program;
+import org.polymodel.polyhedralIR.polyIRCG.generator.CodeGenOptions;
 
 public class GenerateVerificationCode  {
 	
-    
+	/**
+	Generates code for verifying generated program. The code is generated using demand-driven code generator (WriteC).
+	**/
+	public GenerateVerificationCode(@IModuleArgumentName(name="program") Program program,
+@IModuleArgumentName(name="system") String system,
+@IModuleArgumentName(name="system") CodeGenOptions options,
+@IModuleArgumentName(name="outDir") String outDir) {
+	    try {
+		    alphaz.mde.CodeGen.generateVerificationCode(program,system,options,outDir);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		    //System.out.println("Unexpected Exception");
+		    //e.printStackTrace();
+		    throw new RuntimeException("Exception to terminate script");
+		}
+	}//function
+
 	/**
 	Generates code for verifying generated program. The code is generated using demand-driven code generator (WriteC).
 	**/
